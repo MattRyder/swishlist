0594ac19e589fa42b57c473ce19172b7e137a376
Matt Ryder <matt@mattryder.co.uk>
2016-11-14 19:18:30 +0000

    Add Wishlist#Share route

--- Swishlist/App_Start/RouteConfig.cs
+++ Swishlist/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Swishlist
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "WishlistShareRoute",
+                "Wishlists/Share/{cachedSlug}",
+                new { controller = "Wishlists", action = "Share", cachedSlug = "" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
